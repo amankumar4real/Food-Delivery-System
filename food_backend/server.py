c5d4ad3bb2e898c93495946b30bb73b1ed928bfc
Aman Kumar <amankumar4real@gmail.com>
2020-06-21 22:40:52 +0530

    UPDATE: Some change in backend and change of OS

--- food_backend/server.py
+++ food_backend/server.py
@@ -7,7 +7,7 @@ import datetime
 app = Flask(__name__)
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "hiaman1234"
+app.config["MYSQL_PASSWORD"] = "Hiaman@1234"
 app.config["MYSQL_DB"] = "food_del"
 
 mysql = MySQL(app)
